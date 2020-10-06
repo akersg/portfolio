76a07d84d8684d1c262dabc489f35b4da7e17810
akersg <gareth.akers@hotmail.com>
2020-10-06 12:05:39 +0100

    Testing

---
+++ checkwin.js
@@ -0,0 +1,16 @@
+function checkWin(winningCombos, movesArray) {
+    for (let i = 0; i < winningCombos.length; i++) {
+      let count = 0
+      for (let j = 0; j < winningCombos[i].length; j++) { 
+        if (movesArray.includes(winningCombos[i][j])){
+          count++
+          if(count === 3){
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  module.exports = checkWin;
